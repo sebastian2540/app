31b3ccc088e12e17c0b1e9c4c404e3c4f2305ea7
Sebastian Villada Jaramillo <sebastian.villada.jaramillo@gmail.com>
2024-04-18 20:52:57 -0500

    [ADD] se agrega el consumo de api desde el back y se hace uso en el iniciar sesión para el logueo

--- front/src/layouts/components/auth/Login.jsx
+++ front/src/layouts/components/auth/Login.jsx
@@ -1,49 +1,67 @@
 import "./Login.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+let apiUsuarios = "http://localhost:5174/usuarios";
 
 const Login = () => {
   const [getUsuario, setUsuario] = useState("admin");
   const [getContrasena, setContrasena] = useState("");
+  const [getUsuariosBD, setUsuariosBD] = useState([]);
+
   let redireccion = useNavigate();
 
-  if (getUsuario === "sebastian") {
-    if(getContrasena === "123456"){
-      setTimeout(() => {
-        redireccion("/home")
-      },2000);
+  async function traerUsuarios() {
+    let resultado = await axios.get(apiUsuarios);
+    setUsuariosBD(resultado.data)
+  }
+
+  function buscarUsuario() {
+    let estado = getUsuariosBD.some((usuario) => getUsuario === usuario.usuario && usuario.contrasena)
+    return estado
+  }
+
+  function iniciarSesion() {
+    if (buscarUsuario()) {
+      if (buscarUsuario()) {
+        redireccion("/home");
+      }
+    } else {
+      console.log("Error de credenciales");
+      traerUsuarios()
     }
-  } else {
-    
   }
 
   return (
-    <form action="">
-      <h5>Inicio Sesión</h5>
-      <section >
-        <input
-          onChange={(e) => {
-            setUsuario(e.target.value);
-          }}
-          placeholder="Usuario"
-          type="text"
-        />
-        <input
-          onChange={(e) => {
-            setContrasena(e.target.value);
-          }}
-          placeholder="Contraseña"
-          type="password"
-        />
-      </section>
-
-      <section>
-        <button type="button">Iniciar sesión</button>
-        <button type="button">Crear cuentas</button>
-      </section>
-    </form>
-
-    
+    <div className="login-container">
+      <h2>Iniciar Sesión</h2>
+      <form>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            onChange={(e) => {
+              setUsuario(e.target.value);
+            }}
+            placeholder="Usuario"
+            type="text"
+          />
+        </div>
+        <div className="form-group">
+          <label>Contraseña</label>
+          <input
+            onChange={(e) => {
+              setContrasena(e.target.value);
+            }}
+            placeholder="Contraseña"
+            type="text"
+          />
+        </div>
+        <button onClick={iniciarSesion} type="button">
+          Iniciar Sesión
+        </button>
+      </form>
+    </div>
   );
 };
 
